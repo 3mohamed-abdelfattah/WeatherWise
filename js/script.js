ef93a7682a2a9701d779ea85515cb5d7c6eb823f
MohamedMohamed <m01126165016@icloud.com>
2024-08-19 06:43:40 +0300

    Solve issue

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 Geolocation.getCurrentPosition()
+
+async function getData() {
+    const url = "https://example.org/products.json";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        console.log(json);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
